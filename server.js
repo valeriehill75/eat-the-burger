90ba3aa32ee7c2d2409da67ac31f7ed9e4551a17
Valerie Hill <itsvhill@gmail.com>
2020-06-14 14:43:26 -0500

    created server

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = required("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+//Parse application body as JSON.
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set Handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+//Import routes/give server access.
+const routes = require(".controllers/burgerControllers.js");
+
+app.use(routes);
+
+//Start server
+app.listen(PORT, function() {
+    //Log when server has started.
+    console.log("Server listening on: http://localhost:" + PORT);
+});
